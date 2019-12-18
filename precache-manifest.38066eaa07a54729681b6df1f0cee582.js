7365d5aafa8ede4b64a02dcb0205175538a82ad5
a <a@a.pl>
2019-12-18 22:07:43 +0100

    Updates

---
+++ precache-manifest.38066eaa07a54729681b6df1f0cee582.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d6c70c6c3c415a2e4e4774878c7ad3a",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "e129ca0dc9e1ce8ad6b6",
+    "url": "/first-react-app/static/css/main.58bb59ad.chunk.css"
+  },
+  {
+    "revision": "bdd059126d83ada95787",
+    "url": "/first-react-app/static/js/2.0911a306.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/first-react-app/static/js/2.0911a306.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e129ca0dc9e1ce8ad6b6",
+    "url": "/first-react-app/static/js/main.5d60a39c.chunk.js"
+  },
+  {
+    "revision": "12f5e62c87401829e954",
+    "url": "/first-react-app/static/js/runtime-main.3391bd7e.js"
+  }
+]);
